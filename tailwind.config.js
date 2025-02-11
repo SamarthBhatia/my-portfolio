310e0af30ea88c2854504dde9b4fe4ed5d3ba63c
SamarthBhatia <samarthbhatia700@gmail.com>
2025-02-12 01:06:10 +0530

    Initial Setyp

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          primary: "#00ADB5",
+          secondary: "#393E46",
+          dark: "#222831",
+        },
+        fontFamily: {
+          sans: ['Inter', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  }
